ca8c755a5246808b6f630b4368bbeb481ce8c4e9
PayRequest <abuse@hostingwalk.com>
2020-02-03 08:52:34 +0100

    Update aws-env.sh

--- etc/restic/aws-env.sh
+++ etc/restic/aws-env.sh
@@ -3,4 +3,4 @@
 export AWS_ACCESS_KEY_ID="IDhere"
 export AWS_SECRET_ACCESS_KEY="Keyhere"
 export RESTIC_PASSWORD="passwordhere"
-export RESTIC_REPOSITORY="s3:https://s3.amazonaws.com/bucketname"
+export RESTIC_REPOSITORY="s3:https://fra1.digitaloceanspaces.com/walkbackups2/edit"
