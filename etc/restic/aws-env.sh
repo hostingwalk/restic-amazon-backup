ebd55b959a542dd47fe17ee12bea65a83342c734
Hostingwalk <abuse@hostingwalk.com>
2020-03-02 23:25:28 +0000

    Update

--- etc/restic/aws-env.sh
+++
@@ -1,6 +0,0 @@
-# AWS & Restic credentials.
-
-export AWS_ACCESS_KEY_ID="IDhere"
-export AWS_SECRET_ACCESS_KEY="Keyhere"
-export RESTIC_PASSWORD="passwordhere"
-export RESTIC_REPOSITORY="s3:https://fra1.digitaloceanspaces.com/walkbackups2/edit"
--- etc/restic/env.sh
+++ etc/restic/env.sh
@@ -1,5 +1,5 @@
-# AWS & Restic credentials.
-export AWS_ACCESS_KEY_ID="IDhere"
-export AWS_SECRET_ACCESS_KEY="Keyhere"
-export RESTIC_PASSWORD="passwordhere"
-export RESTIC_REPOSITORY="s3:https://fra1.digitaloceanspaces.com/walkbackups2/hostname?"
+# S3 & Restic credentials.
+export AWS_ACCESS_KEY_ID="ID"
+export AWS_SECRET_ACCESS_KEY="Key"
+export RESTIC_PASSWORD="password"
+export RESTIC_REPOSITORY="s3:https://fra1.digitaloceanspaces.com/walkbackups$/hostname$"
