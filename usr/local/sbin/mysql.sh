143ec7ad6a58f12eb45a780cd539895307f8dee3
PayRequest <abuse@hostingwalk.com>
2023-04-29 13:45:04 -0500

    .gz to .zst compression
    .zst is 10 times faster and smaller backups

--- usr/local/sbin/mysql.sh
+++ usr/local/sbin/mysql.sh
@@ -14,14 +14,15 @@ elif [[ ! -d ${DB_BACKUP_PATH} ]]; then
     echo "${DB_BACKUP_PATH} already exists but is not a directory" 1>&2
 fi
 
-find ${DB_BACKUP_PATH} -name '*.gz' -type f -mtime +${BACKUP_RETAIN_DAYS} -exec rm -f {} \;
+find ${DB_BACKUP_PATH} -name '*.zst' -type f -mtime +${BACKUP_RETAIN_DAYS} -exec rm -f {} \;
 
 databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump -u $USER -p$PASSWORD --databases $db | gzip > ${DB_BACKUP_PATH}/${TODAY}.$db.sql.gz
+        #mysqldump -u $USER -p$PASSWORD --databases $db | gzip > ${DB_BACKUP_PATH}/${TODAY}.$db.sql.gz
+        mysqldump -u $USER -p$PASSWORD --databases $db | zstd > ${DB_BACKUP_PATH}/${TODAY}.$db.sql.zst
        # gzip $OUTPUT/`date +%Y%m%d`.$db.sql
     fi
 done
